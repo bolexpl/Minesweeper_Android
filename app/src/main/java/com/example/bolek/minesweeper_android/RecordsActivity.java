2a8b249e8f8fa110e6ca8d1230ac5536ed330db3
bolek <arek2159@gmail.com>
2017-06-29 12:58:48 +0200

    recycler

--- app/src/main/java/com/example/bolek/minesweeper_android/RecordsActivity.java
+++ app/src/main/java/com/example/bolek/minesweeper_android/RecordsActivity.java
@@ -2,9 +2,12 @@ package com.example.bolek.minesweeper_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 public class RecordsActivity extends AppCompatActivity {
+
+    private RecyclerView recycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +15,6 @@ public class RecordsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_records);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
+        recycler = (RecyclerView) findViewById(R.id.recycler);
     }
 }
